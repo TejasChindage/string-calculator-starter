198d66d6d48de522072d11a5a8c60e775b45bfeb
Tejas Chindage <chindagetejas@gmail.com>
2021-10-16 19:41:17 +0530

    Empty string source code

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,13 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+    	//If input is empty return 0
+    	if(input.isEmpty()) 
+    		return 0;
+    	
+    	return -1;
     }
+    
+    
 
 }
