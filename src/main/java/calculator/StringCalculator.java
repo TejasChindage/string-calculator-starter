f517871b7d12178681ad37a0cb4b79cc3ed2bea3
Tejas Chindage <chindagetejas@gmail.com>
2021-10-17 11:54:55 +0530

    String with two number test case

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,36 @@
 package calculator;
 
+import java.util.StringTokenizer;
+
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+    	
+    	//If input is empty then return 0
+    	if(input.isEmpty())
+    		return 0;
+    	else if(input.length() == 1) {
+    		
+    		//If input is single digit 
+    		// return that digit
+    		
+    		return Integer.parseInt(input);
+    	}else if(input.contains(",")) {
+    		
+    		//If input contains , 
+    		//separate no by 
+    		
+    		StringTokenizer token = new StringTokenizer(input, ",");
+    		int sum = 0;
+    		while(token.hasMoreElements()) {
+    			sum  = sum + Integer.parseInt(token.nextToken());
+    			
+    		}
+    		
+    		return sum;
+    	}else {
+    		return -1;
+    	}
     }
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -5,16 +5,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
-
+	
+	//Empty String test case
     @Test
     void empty_string_should_return_0() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
-
+    
+    //String with single numbers test case
     @Test
     void string_with_single_number_should_return_number_as_int() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+    
+    //String with two numbers test case
+    @Test
+    void string_with_two_number_should_return_sum_of_two_number() {
+    	 StringCalculator stringCalculator = new StringCalculator();
+    	 assertEquals(15, stringCalculator.add("13,2"));
+    }
+    
+    
 }
